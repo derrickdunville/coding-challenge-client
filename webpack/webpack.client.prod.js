3db97b52260d4ece362c9b2afa97646d7142bbe8
Derrick Dunville <derrickdunville@gmail.com>
2019-03-28 22:46:49 -0400

    Looks like there is an issue with babel on heroku.

--- webpack/webpack.client.prod.js
+++ webpack/webpack.client.prod.js
@@ -12,7 +12,8 @@ module.exports = merge(common,
     plugins: [
       new webpack.DefinePlugin({ // <-- key to reducing React's size
         'process.env': {
-          'NODE_ENV': JSON.stringify('production')
+          'NODE_ENV': JSON.stringify('production'),
+          'PORT': JSON.stringify(process.env.PORT)
         }
       }),
       new CompressionPlugin({
