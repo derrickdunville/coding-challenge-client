30d25da65e62e6461f2e2fea3f41c2439fa9fc47
Derrick Dunville <derrickdunville@gmail.com>
2019-04-03 02:23:34 -0400

    WOOF! Redux-From initalValues from props. This deserves an SO post for sure.

--- src/components/EditDialog.jsx
+++ src/components/EditDialog.jsx
@@ -8,22 +8,64 @@ import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import { putLink } from '../actions/linkActions'
+import { Field, reduxForm, formValueSelector  } from 'redux-form'
+
+const alphaNumericUnderscoreRegex = new RegExp("^[a-zA-Z0-9_]+$")
+const selector = formValueSelector('editLink')
+
+const validate = values => {
+  const errors = {}
+  if (!values['title']) {
+    errors['title'] = 'Link title is required'
+  }
+  if (values.title && !alphaNumericUnderscoreRegex.test(values.title)) {
+    errors.title = 'Must contain only alphanumeric characters and underscores'
+  }
+  return errors
+}
+
+const renderTextField = ({
+  label,
+  type,
+  input,
+  meta: { touched, invalid, error },
+  ...custom
+}) => {
+  console.log("input: ")
+  console.dir(input)
+  return(
+    <TextField
+      style={{marginTop: "0px", marginBottom: "0px"}}
+      label=""
+      margin="normal"
+      placeholder={label}
+      variant="outlined"
+      shrunk="true"
+      inputProps={{
+        ...input
+      }}
+      helperText={touched && error}
+      error={touched && invalid}
+    />
+  )
+}
+
 
 class EditDialog extends React.Component {
   state = {
     open: false,
     title: this.props.link.title
   }
-
-  handleChange = (event) => {
-    if(event.target.name === 'title'){
-      this.setState({ title: event.target.value })
-    }
+  componentDidMount(){
+    console.log("component did mount")
   }
 
-  handleSave = () => {
-    this.props.putLink(this.props.link.title, this.state.title)
-    this.handleClose()
+  handleSave = (event) => {
+    event.persist()
+    event.preventDefault()
+    console.dir(this.props.title)
+    // this.props.putLink(this.props.link.title, this.state.title)
+    // this.handleClose()
   }
 
   handleClickOpen = () => {
@@ -35,6 +77,9 @@ class EditDialog extends React.Component {
   }
 
   render() {
+    const { title } = this.props
+    console.log("initialValues: ")
+    console.dir(this.props)
     return (
       <div>
         <Button color="default" onClick={this.handleClickOpen}>
@@ -47,20 +92,9 @@ class EditDialog extends React.Component {
         >
           <DialogTitle id="form-dialog-title">Edit {this.props.link.title}</DialogTitle>
           <DialogContent>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="title"
-              label=""
-              type="text"
-              fullWidth
-              variant="outlined"
-              inputProps={{
-                name: 'title',
-                value: this.state.title,
-                onChange: this.handleChange
-              }}
-            />
+            <form onSubmit={this.handleSave}>
+              <Field name="title" label="Title" component={renderTextField} type="text"/>
+            </form>
           </DialogContent>
           <DialogActions>
             <Button onClick={this.handleClose} color="secondary">
@@ -76,10 +110,26 @@ class EditDialog extends React.Component {
   }
 }
 
-function mapStateToProps(state) {
+const mapStateToProps = (state, props) => {
+  console.log("props.link.title: ", props.link.title)
   return {
-
+    initialValues: {
+      title: props.link.title
+    },
+    title: selector(state, 'title')
   }
 }
 
-export default connect(mapStateToProps, { putLink })(EditDialog)
+EditDialog = reduxForm({
+  form: 'editLink',
+  validate,
+  enableReinitialize: true
+})(EditDialog)
+
+EditDialog = connect(
+  mapStateToProps,
+  { putLink }
+)(EditDialog)
+
+
+export default EditDialog
